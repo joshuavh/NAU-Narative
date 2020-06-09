804dc57bb7e5f0e51458703eee927ae904c02dd6
Joshua <joshua.vonhofen@googlemail.com>
2020-06-09 12:33:30 +0200

    Timline Updated

--- js/progressbar.js
+++ js/progressbar.js
@@ -18,4 +18,52 @@ function myFunction() {
         scrolled = (winScroll / width) * 100;
     }
     document.getElementById("myBar").style.width = scrolled * 1 + "%";
+
+      if (scrolled > 1.4) {
+          $("#marker0").css('background', '#7000FF');
+      } else {
+          $("#marker0").css('background', '#4a4a4a');
+      }
+
+      if (scrolled > 16.5) {
+          $("#marker1").css('background', '#7000FF');
+      } else {
+          $("#marker1").css('background', '#4a4a4a');
+      }
+
+      if (scrolled > 35) {
+          $("#marker1a").css('background', '#7000FF');
+      } else {
+          $("#marker1a").css('background', '#4a4a4a');
+      }
+
+      if (scrolled > 46) {
+          $("#marker1b").css('background', '#7000FF');
+      } else {
+          $("#marker1b").css('background', '#4a4a4a');
+      }
+
+      if (scrolled > 52.5) {
+          $("#marker2").css('background', '#7000FF');
+      } else {
+          $("#marker2").css('background', '#4a4a4a');
+      }
+
+      if (scrolled > 67.5) {
+          $("#marker2a").css('background', '#7000FF');
+      } else {
+          $("#marker2a").css('background', '#4a4a4a');
+      }
+
+      if (scrolled > 87.5) {
+          $("#marker2b").css('background', '#7000FF');
+      } else {
+          $("#marker2b").css('background', '#4a4a4a');
+      }
+
+      if (scrolled > 95) {
+          $("#marker3").css('background', '#7000FF');
+      } else {
+          $("#marker3").css('background', '#4a4a4a');
+      }
 }
