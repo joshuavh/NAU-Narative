5d9c56eedd0ad26c2f245165675884ffc60a286b
Joshua <joshua.vonhofen@googlemail.com>
2020-06-10 18:04:50 +0200

    Scroll Speed

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 			$('html, body, *').mousewheel(function(e, delta) {
-				this.scrollLeft -= (delta * 0.5);
+				this.scrollLeft -= (delta * 0.7);
 				// e.preventDefault();
 			});
 
