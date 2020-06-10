bdd86caaf45539302aefba70826a363fc08687ef
Joshua <joshua.vonhofen@googlemail.com>
2020-06-10 10:41:49 +0200

    X fixed

--- js/about.js
+++ js/about.js
@@ -4,11 +4,11 @@ function myButton() {
     x.style.display = "none";
     document.getElementById("about").innerHTML = "about";
     document.getElementById("about").style.fontSize = "20px";
-    document.getElementById("about").style.borderBottom = "1px";
+    document.getElementById("about").style.borderBottomWidth = "1px";
   } else {
     x.style.display = "block";
     document.getElementById("about").innerHTML = "&#10005;";
     document.getElementById("about").style.fontSize = "40px";
-    document.getElementById("about").style.borderBottom = "0px";
+    document.getElementById("about").style.borderBottomWidth = "0px";
   }
 }
